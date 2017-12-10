3baa213d1cfe4a9339a42111358567abdbdfab92
chocolateboy <chocolate@cpan.org>
2017-12-10 19:48:12 +0000

    distribute as ES5 to simplify bundling
    - [BREAKING CHANGE] uses a default export so users can easily choose a different name
    - runs tests on LTS versions of Node
    - update test dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 'use strict'
 
-export function alias (...aliases) {
+var toArray = [].slice
+
+module.exports = function alias () {
+  var aliases = toArray.call(arguments)
   return function (target, name, descriptor) {
-    aliases.forEach(alias => {
+    aliases.forEach(function (alias) {
       Object.defineProperty(target, alias, descriptor)
     })
   }
